f5629880462345acf146c80a876c667a3f3f8eef
douae <douae@kali.douae>
2026-07-12 07:36:42 -1000

    exercice 08

---
+++ Exams/level00/maff_alpha.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+int     main(void)
+{
+        char alpha;
+        char alpha2;
+        int c;
+        c = 0;
+        while(c < 26)
+        {
+                if(c % 2 == 0)
+                {
+                        alpha = 'a' + c;
+                        write(1, &alpha, 1);
+                }
+                else
+                {
+                        alpha2 = 'A' + c;
+                        write(1, &alpha2, 1);
+                }
+                c++;
+        }
+        write(1, "\n", 1);
+        return (0);
+}
+
