32d75bf28d0eb3524844dbd8ebcf6043f664285b
douae <douae@kali.douae>
2026-07-12 05:32:57 -1000

    exercice 01

---
+++ Exams/level00/aff_first_param.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+int	main(int argc, char **argv)
+{
+	if(argc == 1)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	int i;
+	i = 0;
+	while(argv[1][i] != '\0')
+	{
+		i++;
+	}
+	write(1, argv[1], i);
+	write(1, "\n", 1);
+	return (0);
+}
